8a3902e4a57463040461388e5143fd83abc55974
Antonio Malara <antonio.malara@gmail.com>
2013-07-15 22:19:16 +0200

    lil refactoring, and let's see what's going on a fb post

--- sticazzi.py
+++ sticazzi.py
@@ -1,29 +1,33 @@
 import facebook
 import ConfigParser, os, sys
-import pprint
+from pprint import pprint
 
-config = ConfigParser.ConfigParser()
-oauth_access_token = None
 
-try:
-    config.read(os.path.expanduser('~/.sticazzi.cfg'))
-    oauth_access_token = config.get('auth', 'oauth_token')
-except:
-    oauth_access_token = raw_input('Please insert your oauth access token:')
+if __name__ == '__main__':
     config = ConfigParser.ConfigParser()
-    config.add_section('auth')
-    config.set('auth','oauth_token', oauth_access_token)
+    oauth_access_token = None
 
-    with open(os.path.expanduser('~/.sticazzi.cfg'), 'wb') as configfile:
-        config.write(configfile)
+    try:
+        config.read(os.path.expanduser('~/.sticazzi.cfg'))
+        oauth_access_token = config.get('auth', 'oauth_token')
+    except:
+        oauth_access_token = raw_input('Please insert your oauth access token:')
+        config = ConfigParser.ConfigParser()
+        config.add_section('auth')
+        config.set('auth','oauth_token', oauth_access_token)
 
-if oauth_access_token is None:
-    sys.exit(1)
+        with open(os.path.expanduser('~/.sticazzi.cfg'), 'wb') as configfile:
+            config.write(configfile)
 
-graph = facebook.GraphAPI(oauth_access_token)
-news_feed = graph.get_connections("me", "home")
-for new in news_feed['data']:
-    if new['type'] == 'status':
-        print new['from']['name'], '\'s status:', new['message']
-    if new.has_key('caption'):
-        print new['from']['name'],'posted a', new['type'], new['caption']
+    if oauth_access_token is None:
+        sys.exit(1)
+
+    graph = facebook.GraphAPI(oauth_access_token)
+    news_feed = graph.get_connections("me", "home")
+    for new in news_feed['data']:
+    #    if new['type'] == 'status':
+    #        print new['from']['name'], '\'s status:', new['message']
+    #    elif new.has_key('caption'):
+    #        print new['from']['name'],'posted a', new['type'], new['caption']
+    #    else:
+            pprint(new)
