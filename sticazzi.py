e43497d854171d925a073b054246a7aff7138e5a
Antonio Malara <antonio.malara@gmail.com>
2013-07-16 01:19:11 +0200

    a bit of colors in our lifes

--- sticazzi.py
+++ sticazzi.py
@@ -1,9 +1,56 @@
+import os
+import sys
 import facebook
-import ConfigParser, os, sys
-from pprint import pprint
+import textwrap
 
+BASE0=244
+BASE01=240
+YELLOW=136
+ORANGE=166
+RED=160
+MAGENTA=125
+VIOLET=61
+BLUE=33
+CYAN=37
+GREEN=64
+
+def fg256(x, y):
+    return u'\033[38;5;%dm%s\033[0m' % (x, y)
+
+def print_post(post):
+    p = u''
+    p += fg256(CYAN, post['from']['name'])   
+    
+    try: p += ' ' + fg256(BASE0, post['status_type'])
+    except KeyError: pass
+     
+    try: p += '\n' + post['message']
+    except KeyError: pass
+    
+    try: p += '\n' + fg256(YELLOW, post['name'])
+    except KeyError: pass
+    
+    try: p += '\n' + fg256(BASE01, post['description'])
+    except KeyError: pass
+
+    try: likes = post['likes']['count']
+    except: likes = 0
+
+    try: comments = len(post['comments']['data'])
+    except: comments = 0
+    
+    p += '\n'
+
+    p += fg256(BASE0, u'Likes: ') 
+    p += fg256(GREEN, str(likes))
+    p += fg256(BASE0, u' - Comments: ') 
+    p += fg256(GREEN, str(comments))
+
+    print p + '\n'
 
 if __name__ == '__main__':
+    import ConfigParser
+
     config = ConfigParser.ConfigParser()
     oauth_access_token = None
 
@@ -25,9 +72,4 @@ if __name__ == '__main__':
     graph = facebook.GraphAPI(oauth_access_token)
     news_feed = graph.get_connections("me", "home")
     for new in news_feed['data']:
-    #    if new['type'] == 'status':
-    #        print new['from']['name'], '\'s status:', new['message']
-    #    elif new.has_key('caption'):
-    #        print new['from']['name'],'posted a', new['type'], new['caption']
-    #    else:
-            pprint(new)
+        print_post(new)
